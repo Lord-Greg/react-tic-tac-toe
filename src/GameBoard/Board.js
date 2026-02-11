c5789207e4d075787900a671dd87b547a76923a6
Greg <stepniewski.greg@gmail.com>
2026-02-12 00:18:47 +0100

    Missing unique ids for game board's elements.

--- src/GameBoard/Board.js
+++ src/GameBoard/Board.js
@@ -2,21 +2,22 @@ import Field from "./Field";
 
 export default function Board({boardHeight, boardWidth, activePlayer, onExecuteTurn}){
     let lastFieldId = 0; // We'll start our ids with "1".
+    let lastRowId = 0; // Same here.
     let boardRows = [];
     
     function createColumns(columnsNumber){
         let columnsCollection = [];
         
         for (let columnIndex = 0; columnIndex < columnsNumber; columnIndex++) {
-            columnsCollection.push(<Field id={++lastFieldId} activePlayer={activePlayer} onExecuteTurn={onExecuteTurn}/>);
+            columnsCollection.push(<Field key={++lastFieldId} id={lastFieldId} activePlayer={activePlayer} onExecuteTurn={onExecuteTurn}/>);
         }
-
+        
         return columnsCollection;
     }
     
     for (let rowIndex = 0; rowIndex < boardHeight; rowIndex++) {
         boardRows.push(
-            <div className="board-row">
+            <div key={++lastRowId} className="board-row">
                 {createColumns(boardWidth)}
             </div>
         );
