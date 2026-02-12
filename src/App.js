6b930e8b2b2a3eaa796c6bd1d418891dc0332f1e
Greg <stepniewski.greg@gmail.com>
2026-02-12 23:30:14 +0100

    Logic for checking a left-to-right slope winner.

--- src/App.js
+++ src/App.js
@@ -77,7 +77,50 @@ export default function Game() {
 		}
 		
 		function checkSlopes() {
-			// TODO: Implement logic.
+			// In this app, we won't be sure if the board is actually a square (e.g. 3x3), or a rectangle (e.g. 3x4).
+			// Our "slope winner" will be determined by having as match hits in a single slope, as the board allows.
+			// E.g. for 3x3 max possible slope is 3, for 4x5 it's 4, and for 5x10 it's 5.
+
+			const winningSlopeSize = Math.min(boardWidth, boardHeight);
+			let winner = null;
+
+			function checkIfWinningSlopeIsPossible(rowIndex, columnIndex) {
+				return (boardHeight - rowIndex >= winningSlopeSize) || (boardWidth - columnIndex >= winningSlopeSize);
+			}
+
+			function checkNextElementInSlope(testedValue, previousRowIndex, previousColumnIndex) {
+				const currentRowIndex = previousRowIndex + 1;
+				const currentColumnIndex = previousColumnIndex + 1;
+				
+				return (currentRowIndex >= boardHeight || currentColumnIndex >= boardWidth)
+					|| (fieldsValues[currentColumnIndex + (currentRowIndex * boardWidth)] === testedValue
+						&& checkNextElementInSlope(testedValue, currentRowIndex, currentColumnIndex));
+			}
+
+			// Step 1: Checking left-to-right slopes.
+			// In fact, we only need to iterate through a first row, and a first column.
+			// The recursion of checkNextElementInSlope() function will handle rest fo the job.
+			
+			for (let columnIndex = 0; columnIndex < boardWidth; columnIndex++) {
+				if(fieldsValues[columnIndex] !== null && checkNextElementInSlope(fieldsValues[columnIndex], 0, columnIndex)) {
+					winner = fieldsValues[columnIndex];
+					break;
+				}
+			}
+
+			if(winner === null) {
+				for (let rowIndex = 0; rowIndex < boardHeight; rowIndex++) {
+					if(fieldsValues[rowIndex * boardWidth] !== null && checkNextElementInSlope(fieldsValues[columnIndex * boardWidth], rowIndex, 0)) {
+						winner = fieldsValues[columnIndex * boardWidth];
+						break;
+					}
+				}
+			}
+
+			// Step 2: Checking right-to-left slopes.
+			// TODO: Implementation.
+
+			return winner;
 		}
 
 		return checkRows() ?? checkColumns() ?? checkSlopes();
